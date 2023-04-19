87ccad64fa784e8c6c67444ab5b6802ef13b482a
JD Tadlock <jt7903@gmail.com>
2023-04-19 09:18:46 -0400

    updated public routes to with redirect

--- controllers/public_routes.js
+++ controllers/public_routes.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
 
+// Middleware function to redirect a logged in user to the dashboard
+// We don't want to show auth pages if they're logged in
+function isLoggedIn(req, res, next) {
+  if (req.session.user_id) return res.redirect('/dashboard');
+
+  next();
+}
+
 // Render the Homepage view
-router.get('/', (req, res) => {
+router.get('/', isLoggedIn, (req, res) => {
   res.render('index');
 });
 
 // Render the Login Page view
-router.get('/login', (req, res) => {
+router.get('/login', isLoggedIn, (req, res) => {
   res.render('auth/login');
 });
 
 // Render the Register Page view
-router.get('/register', (req, res) => {
+router.get('/register', isLoggedIn, (req, res) => {
   res.render('auth/register');
 });
 
